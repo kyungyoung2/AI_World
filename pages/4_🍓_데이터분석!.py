64282ccd4f7c94a66fd6b004ed38e06b3b3b58a5
kyungyoung2 <kyungyounghs@gmail.com>
2023-08-04 06:56:21 +0900

    8/3

--- pages/4_🍓_데이터분석!.py
+++ pages/4_🍓_데이터분석!.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+from streamlit_drawable_canvas import st_canvas
 
 
 st.title("데이터분석")
 
 # 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다.
-tab1, tab2, tab3= st.tabs(['성적분석  ', '삼성전 주식 조회  ', 'MNIST'])
+tab1, tab2, tab3= st.tabs(['성적분석  ', '삼성전자 주식 조회  ', 'MNIST'])
 
 with tab1:
     # tab A 를 누르면 표시될 내용
@@ -29,6 +33,7 @@ with tab1:
     ax.bar(data['이름'], data['국어'])
     st.pyplot(fig)
 
+
 with tab2:
     # tab B를 누르면 표시될 내용
     # Finance Data Reader
@@ -36,8 +41,48 @@ with tab2:
     st.subheader("조회 시작을 선택하세요.")
 
 
+
 with tab3:
     # tab B를 누르면 표시될 내용
     st.write('비지도 학습')
 
+
+    @st.cache(allow_output_mutation=True)
+    def load():
+        return load_model('model.h5')
+
+
+    model = load()
+
+    st.write('# MNIST Recognizer')
+
+    CANVAS_SIZE = 192
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        canvas = st_canvas(
+            fill_color='#000000',
+            stroke_width=20,
+            stroke_color='#FFFFFF',
+            background_color='#000000',
+            width=CANVAS_SIZE,
+            height=CANVAS_SIZE,
+            drawing_mode='freedraw',
+            key='canvas'
+        )
+
+    if canvas.image_data is not None:
+        img = canvas.image_data.astype(np.uint8)
+        img = cv2.resize(img, dsize=(28, 28))
+        preview_img = cv2.resize(img, dsize=(CANVAS_SIZE, CANVAS_SIZE), interpolation=cv2.INTER_NEAREST)
+
+        col2.image(preview_img)
+
+        x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        x = x.reshape((-1, 28, 28, 1))
+        y = model.predict(x).squeeze()
+
+        st.write('## Result: %d' % np.argmax(y))
+        st.bar_chart(y)
 
