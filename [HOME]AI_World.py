5c4e75a754b566bea6f2b2898c101ed2a843c05d
kyungyoung2 <kyungyounghs@gmail.com>
2023-08-03 16:43:13 +0900

    8/3

--- [HOME]AI_World.py
+++ [HOME]AI_World.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="인공지능이해",
+    page_title="AI WORLD",
     page_icon="👋",
 )
 
@@ -9,7 +9,7 @@ st.subheader("AI World")
 #st.sidebar.success("Select a page above.")
 st.write(" ")
 st.image('ai.jpg')
-st.markdown('인공지능 수업에 오신 것을 **환영합니다.**인공지능 개념,'
+st.markdown('인공지능 수업에 오신 것을 환영합니다.인공지능 개념,'
             '인공지능 사례(생성형 인공지능)'
             '인공지능 윤리등에 대한 내용을 다룹니다.')
 st.markdown("1. **인공지능 개념**: 머신러닝, 딥러닝")
--- pages/2_📚_인공지능개념.py
+++ pages/2_📚_인공지능개념.py
@@ -1,21 +1,27 @@
 import streamlit as st
 
 
-st.title("Projects")
+st.title("인공지능이란?")
 
 # 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다.
-tab1, tab2, tab3 = st.tabs(['지도학습', '비지도학습', '강화학습'])
+tab1, tab2, tab3, tab4= st.tabs(['인공지능 역사','지도학습', '비지도학습', '강화학습'])
 
 with tab1:
     # tab A 를 누르면 표시될 내용
-    st.write('지도학습')
-
+    st.write("")
+    st.subheader('1. 인공지능이란?')
+    st.markdown("인공지능이란 문제 해결을 위해 대상을 **인식**하고 필요한 부분을 **학습**하며, 논리적인 "
+                " **판단**과 **추론**하는 능력을 말한다. ")
+    st.markdown("인공지능(Ari Intelligenc)이란 용어는 1956년 다트머스 컨퍼런스에서 **존 매카시**가 최초로 주장하였다.")
 with tab2:
     # tab B를 누르면 표시될 내용
-    st.write('비지도학습')
+    st.write('지도 학습')
 
 with tab3:
     # tab B를 누르면 표시될 내용
-    st.write('강화학습')
+    st.write('비지도 학습')
 
-st.write("You have entered")
+with tab4:
+    # tab B를 누르면 표시될 내용
+    st.write('강화 학습')
+
