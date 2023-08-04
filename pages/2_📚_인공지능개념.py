3fd785786d017d80e4d26b0ab9db71fb4a2fc55e
kyungyoung2 <kyungyounghs@gmail.com>
2023-08-04 18:39:15 +0900

    8/3

--- pages/2_📚_인공지능개념.py
+++ pages/2_📚_인공지능개념.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("인공지능이란?")
+st.title("인공지능(Artificial Intelligence)이란?")
 
 # 탭 생성 : 첫번째 탭의 이름은 Tab A 로, Tab B로 표시합니다.
 tab1, tab2, tab3, tab4= st.tabs(['인공지능 역사','머신러닝', '딥러닝', '강화학습'])
@@ -34,15 +34,30 @@ with tab1:
     st.caption('2016년 3월 10일 알파고와 두 번째 대국을 하고 있는 이세돌 구단,'
            '출처 http://image.dongascience.com/Photo/2016/03/14575928244225.jpg')
 
-    st.markdown('**⑤ ChatGPT(Chat Generative Pre-trained Trasformer)**: ')
-
+    st.markdown('**⑤ ChatGPT(Chat Generative Pre-trained Trasformer)**: 2022년 11월 30일에 출시되어 현재까지 큰 반향을 일으키고 있는 **생성형 인공지능**이다.'
+                '**Large Language Model**로 자연어 처리에 획기적인 발전을 이루었다는 평을 받으며, 출시 5일만에 사용자 1억명을 돌파했다.'
+                '다른 LLM모델로 구글의 Bard, 메타의 LLaMA, 네이버 하이퍼클로바X 등이 있다. 챗GPT뿐만 아니라 DALL.E, Midjourney 등의 이미지 생성AI도 각광받고 있다.')
+    st.image('https://cdn.eroun.net/news/photo/202304/31633_58340_5223.png')
+    st.caption('출처: OpenAI 홈페이지')
 
 
 with tab2:
     # tab B를 누르면 표시될 내용
     st.write("")
-    st.subheader('지도학습')
+    st.subheader('1.인공지능&머신러닝&딥러닝')
+    st.image("ai_structure.jpg")
+    st.caption("▲인공지능, 머신러닝, 딥러닝의 관계도")
 
+    st.subheader("2. 머신러닝(Machine Learning)")
+    st.markdown('머신러닝은 인공지능을 구현하는 대표적인 방법 중의 하나로, **데이터에서 패턴을 찾아내 문제애 대한 답을 예측하는 알고리즘**이다. '
+                '머신러닝의 활용 분야로 금융, 이미지프로세싱, 금융분석 및 탐지 분야, 음성인식, 로봇제어 분야 등이 있다.')
+    st.write("")
+    st.markdown("머신러닝의 학습 방법에는 지도 학습, 비지도 학습, 강화 학습의 3가지로 나뉠 수 있다.")
+    st.write("")
+    st.markdown("**① 지도 학습(Supervised Learning)** : 지도 학습은 **데이터와 정답**을 함께 제시하여 학습하는 방법이다. 지도 학습에는 분류와 회귀 모델이 있다.")
+    st.markdown("**② 비지도 학습(Unsupervised Learning)** : 비지도 학습은 정답이 없는 **데이터만** 주고 학습하게 방법이다. 정답이 없기 때문에 입력 데이터의 패턴이나 특성을 통해 학습하게 된다.")
+    st.markdown("**③ 강화 학습(Reinforcement Learning)** : 강화 학습은 입력 데이터를 학습하여 **경험과 보상**을 통해 목표값을 찾도록 학습하는 방법이다. 즉 입-출려간의 상관관계를 찾는 것이 아니라, 시행착오에 대한 보상 체계를 바탕으로"
+                "지속적인 경험에 통해 최선책을 찾도록 하는 방법이다. 주로 자율주행자동차나 게임, 주식 등에 활용된다.")
 with tab3:
     # tab B를 누르면 표시될 내용
     st.write('비지도 학습')
