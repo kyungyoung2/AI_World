01e06a4d2a0b73610a21206c95f00c9fc9b06434
kyungyoung2 <kyungyounghs@gmail.com>
2023-08-04 16:51:19 +0900

    8/3

--- pages/3_💬_인공지능모델.py
+++ pages/3_💬_인공지능모델.py
@@ -8,7 +8,7 @@ tab1, tab2= st.tabs(['랜덤포레스트','최근접이웃'])
 with tab1:
     # tab A 를 누르면 표시될 내용
     st.write("")
-    st.subheader('1.램덤포레스트 모델')
+    st.subheader('1.랜덤포레스트 모델')
 
 with tab2:
     # tab B를 누르면 표시될 내용
