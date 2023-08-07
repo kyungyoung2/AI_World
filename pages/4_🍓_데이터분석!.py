d951bdf41c06ef9ab0771c8b0ab654c5e25e64ca
kyungyoung2 <kyungyounghs@gmail.com>
2023-08-07 22:10:31 +0900

    8/3

--- pages/4_🍓_데이터분석!.py
+++ pages/4_🍓_데이터분석!.py
@@ -7,12 +7,36 @@ df = pd.read_csv("iris.csv")
 
 tab1, tab2, tab3, tab4 = st.tabs(['붓꽃 데이터란?', '붓꽃 데이터 보기','붓꽃 데이터 필터링','붓꽃 데이터 시각화'])
 with tab1 :
-    st.subheader("붓꽃(Iris) 데이터란?")
-    st.markdown('''
-        안녕하세요.
-        마크다운입니다.
-        ''')
-    st.write("곧 업데이트 중... 붓꽃 데이터 보기, 필터링, 시각화를 먼저 보세요!")
+    st.subheader("1. 붓꽃(Iris) 데이터 세트란?")
+    st.markdown("""
+        <p style="background-color:#EAEAEA">
+            통계학자 R.A. 피셔가 1936년에 소개한 데이터 세트로, <b><font color="red">세 가지 다른 품종의 붓꽃(Setosa, Versicolor, Virginica)</font></b>의 
+            형태적 특징으로 이루어진 수집한 <b>표준 데이터 세트</b>이다.<br>
+            붓꽃 데이터 세트는 붓꽃의 꽃받침과 꽃잎에 대한 측정 자료로 구성된 데이터로, 통계학과 머신 러닝 분야에서 가장 널리 사용되는 
+            데이터 세트들 중 하나이다. 각 샘플에는 <b><font color="blue">꽃잎의 길이와 너비</font></b>, 그리고 <b><font color="blue">꽃받침의 길이와 너비</font></b>에 대한 센티미터(cm) 단위의 측정치와 
+            해당 샘플의 품종이 레이블 형태로 존재한다. 붓꽃 데이터 세트는 통계적 분류 문제의 대표적인 테스트 데이터로 쓰이고 있다.<br>
+            <출처: 두산백과>
+        </p>
+        """, unsafe_allow_html=True)
+    st.image("https://miro.medium.com/max/3500/1*f6KbPXwksAliMIsibFyGJw.png")
+    st.image("https://content.codecademy.com/programs/machine-learning/k-means/iris.svg")
+    st.caption("출처:https://www.kaggle.com/code/sunaysawant/iris-eda-k-means-clustering/")
+
+    st.subheader("2. 붓꽃 데이터 세트의 구성")
+    st.markdown("""
+        <p style = "text-align:center;">
+        <b><붓꽃 데이터 세트의 일부></b>
+        </p>
+    """, unsafe_allow_html=True)
+
+    st.image("images/iris_table.jpg")
+    st.write("① sepal length: 꽃받침 길이")
+    st.write("② sepal width: 꽃받침 너비")
+    st.write("③ petal length: 꽃잎 길이")
+    st.write("④ petal width: 꽃잎 너비")
+    st.write("⑤ species: 품종")
+    st.image("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbeUgB8%2FbtrVkeWxNAE%2FwlewLtdh9dktLOKa4junIk%2Fimg.png")
+
 
 with tab2 :
     st.subheader('1.붓꽃 데이터(상위 5개 데이터)')
@@ -118,9 +142,11 @@ with tab4 :
     ax.hist(x="sepal length", data=tmp_df4)
     st.pyplot(fig)
 
-    code = '''
-            붓꽃 데이터 시각화를 통해 세 가지 품종에서 sepal length의 길이가 차이가 있음을 알 수 있다
-            세토사 품종의 sepal length가 짧은 편이고, 버지니카 품종의 sepal length가 다른 품종보다 조금 더 긴 것으로 나타났다.
-            데이터 시각화를 통해 데이터에 대한 분석을 좀 더 쉽게 할 수 있음을 단적으로 보여준다.
-            '''
-    st.code(code, language="python")
+    st.markdown("""
+            <p style = "background-color:#EAEAEA; color:red;">
+            <br>
+            <b><시각화 후 분석 결과></b><br>
+            붓꽃 데이터 시각화를 통해 세 가지 품종에서 sepal length의 길이가 차이가 있음을 알 수 있다.<br>
+            세토사 품종의 sepal length가 짧은 편이고, 버지니카 품종의 sepal length가 다른 품종보다 조금 더 긴 것으로 나타났다.<br>
+            데이터 시각화를 통해 데이터에 대한 분석을 좀 더 쉽게 할 수 있음을 단적으로 보여준다.<br><br>
+            </p>""", unsafe_allow_html=True)
